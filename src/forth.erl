8f49b5244fad4dc531b418168080d7dc7d8ac24d
John Daily <git@epep.us>
2019-11-10 20:25:14 -0500

    Blog notes and code: sync up to word definition

--- src/forth.erl
+++ src/forth.erl
@@ -20,26 +20,32 @@ maybe_convert_int(Str) ->
 convert_ints(Tokens) ->
     lists:map(fun maybe_convert_int/1, Tokens).
 
-evaluate(Instructions) ->
-    evaluate(parse(Instructions), []).
+default_words() ->
+    dict:from_list(
+      [
+       {"+",     {2,   fun([A, B], W) -> {[B+A], W} end}},
+       {"-",     {2,   fun([A, B], W) -> {[B-A], W} end}},
+       {"*",     {2,   fun([A, B], W) -> {[B*A], W} end}},
+       {"/",     {2,   fun([A, B], W) -> {[B div A], W} end}},
+       {"dup",   {1,   fun([A], W)    -> {[A, A], W} end}},
+       {"drop",  {1,   fun([_A], W)    -> {[], W} end}},
+       {"swap",  {2,   fun([A, B], W) -> {[B, A], W} end}},
+       {"over",  {2,   fun([A, B], W) -> {[B, A, B], W} end}}
+      ]).
 
-evaluate([], Stack) ->
+
+interpret(_Next, {ok, {Arity, Fun}}, RemainingInputs, Stack, Words) ->
+    {Args, StackTail} = lists:split(Arity, Stack),
+    {Result, NewWords} = Fun(Args, Words),
+    {RemainingInputs, Result ++ StackTail, NewWords};
+interpret(Next, error, RemainingInputs, Stack, Words) ->
+    {RemainingInputs, [Next|Stack], Words}.
+
+
+evaluate(Instructions) ->
+    real_evaluate({parse(Instructions), [], default_words()}).
+
+real_evaluate({[], Stack, _Words}) ->
     lists:reverse(Stack);
-evaluate([H|T], Stack) when is_integer(H) ->
-    evaluate(T, [H|Stack]);
-evaluate(["+"|T], [A, B|Stack]) when is_integer(A), is_integer(B) ->
-    evaluate(T, [A+B|Stack]);
-evaluate(["-"|T], [A, B|Stack]) when is_integer(A), is_integer(B) ->
-    evaluate(T, [B-A|Stack]);
-evaluate(["*"|T], [A, B|Stack]) when is_integer(A), is_integer(B) ->
-    evaluate(T, [A*B|Stack]);
-evaluate(["/"|T], [A, B|Stack]) when is_integer(A), is_integer(B) ->
-    evaluate(T, [B div A|Stack]);
-evaluate(["dup"|T], [A|Stack]) ->
-    evaluate(T, [A,A|Stack]);
-evaluate(["drop"|T], [_A|Stack]) ->
-    evaluate(T, Stack);
-evaluate(["swap"|T], [A, B|Stack]) ->
-    evaluate(T, [B, A|Stack]);
-evaluate(["over"|T], [A, B|Stack]) ->
-    evaluate(T, [B, A, B|Stack]).
+real_evaluate({[H|T], Stack, Words}) ->
+    real_evaluate(interpret(H, dict:find(H, Words), T, Stack, Words)).
